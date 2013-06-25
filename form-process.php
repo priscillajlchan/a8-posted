d4aa4c2655bd3b2a0893ec2206351a2903bb094b
Priscilla <priscillachan__@hotmail.com>
2013-06-24 20:47:54 -0700

    First Commit
    intro to PHP

---
+++ form-process.php
@@ -0,0 +1,28 @@
+<?php
+
+//form processing script 
+
+$name1 = $_POST["name1"];
+$name2 = $_POST["name2"];
+$verb = $_POST["verb"];
+$title = $_POST["title"];
+$place = $_POST["place"];
+$adverb = $_POST["adverb"];
+
+var_dump($_POST);
+
+
+$message = 
+"This is a story about when ".$name1." met ".$name2."
+They instantly fell in ".$verb.". 
+It wasn't long until he made her his ".$title.".
+Together they moved far away, to a quiet ".$place."
+and lived ".$adverb." ever after. ";
+
+$message = wordwrap ($message, 70, "\r\n");
+
+$headers = "From: priscillajlchan@gmail.com";
+
+mail ("priscillachan__@hotmail.com", "A Mad Lib for You", $message, $headers); 
+
+?> 
